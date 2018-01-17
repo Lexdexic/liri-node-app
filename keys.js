bd2febd8762501c4bc4292fe45978f6174c8cca6
Jeremy <Lexdexic@gmail.com>
2018-01-17 16:34:27 -0700

    liri all sections filled omdb almost complete

--- keys.js
+++ keys.js
@@ -1,17 +1,16 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.MbdznwJm8prTViibDlQIRuZMu,
-  consumer_secret: process.env.z6m6O8ZHDY6Uz8DSteol5n40h7nbGUFEJcInge9weIljqu0Kkz
-,
-  access_token_key: process.env.950454288999890944-qJI5Xs9cgNn51qmOMvqOfXayr7YRRrH,
-  access_token_secret: process.env.eM5xvps3wlwJSTKF7uoE3DlbIigR09TkkVixKTTD8tKbI
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 
 };
 
-exports.spotify = {
-  id: process.env.bba733eef0b4815b7889c81cf99f4,
-  secret: process.env.6249c2683f7c483c9e8222587abb2
+exports.spotify = {  
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
 
 // See instructions lines 79-96 Get your Twitter API keys by following these steps:
--- liri.js
+++ liri.js
@@ -2,15 +2,18 @@
 // spotify same name as installed package
 // OMDB with 
 //   request
-var Env = require("env").config();
+var request = require("request");
+var omdb = require('omdb');
+var Env = require("dotenv").config();
 var Twitter = require("twitter");
-var Spotify = require("spotify");
+var Spotify = require("node-spotify-api");
 var fs = require("fs");
 var keys = require("./keys.js");
 
 // insert keys below
-var spotify = new Spotify(bba733eef0b4815b7889c81cf99f4);
-var twitter = new Twitter(MbdznwJm8prTViibDlQIRuZMu);
+var spotify = new Spotify(keys.spotify);
+
+var twitter = new Twitter(keys.twitter);
 
 function primary(command, searchName){
 	switch(command){
@@ -18,16 +21,17 @@ function primary(command, searchName){
 			// call twitter function
 			getTweets();
 			break;
-		case "spotify-this-song"
+		case "spotify-this-song":
 			// call spotify function pass search name in also
-			seachSong(searchName);
+			searchSpotify(searchName);
 			break;
 		case "movie-this":
 			// call omdb function pass search name in also
-			seachOMDB(searchName);
+			searchOMDB(searchName);
 			break;
 		case "do-what-it-says":
 			// call random function
+			random();
 			break;
 		default:
 			// call random function
@@ -38,10 +42,18 @@ function getTweets(){
 	// show last 20 tweets
 	// show created date consolelog it
 	var params = {screen_name: 'nodejs'};
-	client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  		if (!error) {
-   			 console.log(tweets);
+	twitter.get('statuses/user_timeline', params, function(error, tweets, response) {
+  		if (error) {
+
+  			return console.log('Error occurred: ' + error)
+   			
  		}
+ 		for(var i = 0; i < 20; i++){
+			console.log("This tweet was created on " + tweets[i].created_at);
+			console.log(tweets[i].text);
+			console.log("**************************************************\n");
+  		}
+  		logData(tweets);
 	});
 }
 
@@ -53,15 +65,25 @@ function searchSpotify(songName){
 		// The album that the song is from
 
 	// If no song is provided then your program will default to "The Sign" by Ace of Base.
+	if (!songName){
+		songName = "lose yourself";
+	}
 
-	// instructions:
-		// You will utilize the node-spotify-api package in order to retrieve song information from the Spotify API.
-		// Like the Twitter API, the Spotify API requires you sign up as a developer to generate the necessary credentials. You can follow these steps in order to generate a client id and client secret:
-		// Step One: Visit https://developer.spotify.com/my-applications/#!/
-		// Step Two: Either login to your existing Spotify account or create a new one (a free account is fine) and log in.
-		// Step Three: Once logged in, navigate to https://developer.spotify.com/my-applications/#!/applications/create to register a new application to be used with the Spotify API. You can fill in whatever you'd like for these fields. When finished, click the "complete" button.
-		// Step Four: On the next screen, scroll down to where you see your client id and client secret. Copy these values down somewhere, you'll need them to use the Spotify API and the node-spotify-api package.
+	spotify.search({ type: 'track', query: songName }, function(error, data) {
+		if (error) {
+			return console.log('Error occurred: ' + error);
+		}
 
+		var infoData = data.tracks.items[0];
+		console.log(
+			{songName: infoData.name},
+			{artist: infoData.artists[0].name}, 
+			{albumName: infoData.album.name}, 
+			{preview: infoData.preview_url}
+		);
+		logData(data);
+
+	});
 }
 
 function searchOMDB(movieName){
@@ -75,6 +97,34 @@ function searchOMDB(movieName){
 		// * Plot of the movie.
 		// * Actors in the movie.
 
+
+ 
+omdb.search('saw', function(error, movies) {
+    if(error) {
+        return console.error(err);
+    }
+ 
+    if(movies.length < 1) {
+        return console.log('No movies were found!');
+    }
+ 
+    movies.forEach(function(movie) {
+        console.log('%s (%d)', movie.title, movie.year);
+    });
+});
+ 
+omdb.get({ title: 'Saw', year: 2004 }, true, function(error, movie) {
+    if(err) {
+        return console.error(error);
+    }
+ 
+    if(!movie) {
+        movie = "Mr. Nobody";
+    }
+ 
+    console.log('%s (%d) %d/10', movie.title, movie.year, movie.imdb.rating);
+    console.log(movie.plot);
+
 	// If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
 
 	// instructions:
@@ -82,6 +132,17 @@ function searchOMDB(movieName){
 }
 
 function random(){
+	fs.readFile('random.txt', "utf8", function(error, data) {
+		var options = data.split("/");
+		
+		var option;
+		for (var i = 0; i < options.length; i++) {
+			option = options[Math.floor(Math.random()*options.length)];
+		}
+
+		var args = option.split(",");
+		primary(args[0], args[1]);
+  	});
 	// instructions:
 		// Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands. usr fs to get string varable. split the string variable in to an array. call primary function: the array[0]= command, array[1]= searchName.
 
@@ -89,7 +150,14 @@ function random(){
 		// 	Feel free to change the text in that document to test out the feature for other commands.
 }
 
-function logData(){
+function logData(searchData){
+
+	fs.appendFile('log.txt', searchData, function (error) {
+  		if (error){
+  			return console.log('Error occurred: ' + error);
+  		}
+ 		console.log('Logged!');
+	});
 	// instructions:
 		// 	In addition to logging the data to your terminal/bash window, output the data to a .txt file called log.txt.
 		// Make sure you append each command you run to the log.txt file. 
